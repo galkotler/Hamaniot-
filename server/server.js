8c099112e328545466ca2464c7aa226ac8f5eb60
lironz8 <lironzvaigoft@gmail.com>
2025-06-15 21:15:44 +0300

    f1

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: true, 
+    secure: process.env.NODE_ENV === 'production', 
     httpOnly: true,
     sameSite: 'lax', 
     maxAge: 1000 * 60 * 60 
