487122e84d70d8cca777200b7ee366f4e4892e1d
liron zvaigoft <lironzv@mta.ac.il>
2025-06-29 12:01:26 +0300

    Update volunteer_register.js

--- client/js/volunteer_register.js
+++ client/js/volunteer_register.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = Object.fromEntries(formData.entries());
 
     try {
-      const response = await fetch("${BASE_URL}/api/volunteers", {
+      const response = await fetch(`${BASE_URL}/api/volunteers`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
